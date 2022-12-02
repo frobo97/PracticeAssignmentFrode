ed4d7fcb00841accee12548e718b7f7396158e2e
Frode Lien <79.frode@gmail.com>
2022-12-02 09:31:56 +0100

    initial commit

---
+++ src/main/java/se/jensen/javacourse/week3/frode/PracticeAssignment3/PracticeAssignment3Application.java
@@ -0,0 +1,14 @@
+package se.jensen.javacourse.week3.frode.PracticeAssignment3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeAssignment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeAssignment3Application.class, args);
+
+	}
+
+}
